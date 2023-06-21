d50bb24e711e397fee5688c61e65db9d6f836ff8
unknown <naocenar@gmail.com>
2023-06-22 00:48:32 +0800

    added more responsive layout css

--- components/sections/Footer.tsx
+++ components/sections/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
     <div className='Footer'>
       <h3>Protoapp</h3>
       <h3>Camp One Inc, LLC</h3>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere labore atque inventore quos libero, veritatis ipsam nesciunt, error cum maxime rerum. Velit enim sapiente adipisci qui, iste facilis beatae totam illo in quia quidem placeat, numquam tenetur saepe tempora et unde. Porro culpa perspiciatis repellendus et nulla fugit laudantium sequi, veritatis cumque odit dolorem? Pariatur repudiandae aspernatur ab delectus impedit!</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis earum placeat commodi architecto, ut unde aspernatur sunt. Aut voluptatem ratione ex, recusandae dolorem, laborum architecto minus voluptates praesentium et laboriosam!</p>
       <br />
       <p>@ 2019 Protoapp. All rights reserved.</p>
     </div>
