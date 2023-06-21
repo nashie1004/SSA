ba4f4363351aaada646802969ee6dbe29aa3ebe1
unknown <naocenar@gmail.com>
2023-06-22 01:10:48 +0800

    semi fixed readme, added apostrophe

--- components/sections/SectionThree.tsx
+++ components/sections/SectionThree.tsx
@@ -5,7 +5,7 @@ export default function SectionThree() {
     <div className='SectionThree'>
       <div>
         <h2>Deep dive. Ready?</h2>
-        <p>So are we. Unless. You're. Not filler.</p>
+        <p>So are we. Unless. You&apos;re. Not filler.</p>
         <div>
           <p>google play btn</p>
           <p>app store btn</p>
